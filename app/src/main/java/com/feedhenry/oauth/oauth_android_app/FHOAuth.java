fa5bf011d0307cdd77504711633ec3702ce69f5b
Summers Pittman <secondsun@gmail.com>
2015-10-22 11:11:39 -0400

    AutoLicense

--- app/src/main/java/com/feedhenry/oauth/oauth_android_app/FHOAuth.java
+++ app/src/main/java/com/feedhenry/oauth/oauth_android_app/FHOAuth.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2015 FeedHenry Ltd, All Rights Reserved.
+ *
+ * Please refer to your contract with FeedHenry for the software license agreement.
+ * If you do not have a contract, you do not have a license to use this software.
+ */
 package com.feedhenry.oauth.oauth_android_app;
 
 import android.support.v7.app.AppCompatActivity;
